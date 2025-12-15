4a1afc8fcba77fa875868de1a6b87aac559b18f2
Hieu Nguyen <168950622+wokhyu@users.noreply.github.com>
2025-12-15 16:29:35 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Services from "./components/Services";
-import Booking from "./components/Booking";
-import Testimonials from "./components/Testimonials";
-import Feedback from "./components/Feedback";
-import Footer from "./components/Footer";
-import Navbar from "./components/NavBar";
 
-export default function App() {
+import { Toaster } from "sonner";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import SignupPage from "@/pages/SignupPage";
+import LoginPage from "@/pages/LoginPage";
+function App() {
   return (
-    <div className="min-h-screen font-sans text-slate-900 bg-gradient-to-b from-emerald-50 via-emerald-50 to-emerald-100">
-      <Navbar />
-      <main className="pt-28">
-        <Hero />
-        <About />
-        <Services />
-        <Booking />
-        <Testimonials />
-        <Feedback />
-        <Footer />
-      </main>
-    </div>
+    <>
+      <Toaster richColors />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
+
+export default App;

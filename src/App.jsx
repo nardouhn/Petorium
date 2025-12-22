66e1bafa89ff3559b5aa4fd4283f908663c64ea5
Hieu Nguyen <168950622+wokhyu@users.noreply.github.com>
2025-12-22 20:22:19 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import React from "react";
 
 import { Toaster } from "sonner";
 import { BrowserRouter, Routes, Route } from "react-router";
+import ScrollToHash from "./utils/ScrollToHash";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import SignupPage from "@/pages/SignupPage";
 import LoginPage from "@/pages/LoginPage";
+import ServicePage from "@/pages/ServicePage";
 function App() {
   return (
     <>
       <Toaster richColors />
 
       <BrowserRouter>
+        <ScrollToHash />
         <Routes>
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
 
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/services/:slug" element={<ServicePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
